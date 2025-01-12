b5e965d3580539ffcd90028026326cdc4beb51df
cjluther26 <cjluther26@yahoo.com>
2025-01-11 19:40:23 -0500

    changing to new URL in README, safe-guarding data collector script

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Head over to this [URL](https://tot-new-20241216-xg.streamlit.app/) to see this (very, very, VERY basic) Streamlit app in action!
+Head over to this [URL](https://epl-2024-25-match-xg-map.streamlit.app/) to see this (very, very, VERY basic) Streamlit app in action!
--- get_match_results.py
+++ get_match_results.py
@@ -5,18 +5,20 @@ from utils.understat_utils import (
     create_shots_df_multiple
 )
 
-# Get EPL matches for 2024 season, up to now...
-epl_matches = get_understat_matches(league_id = "EPL", year = 2024)
+if __name__ == "__main__":
 
-# Isolate match IDs
-epl_match_ids = get_match_ids(epl_matches)
-# epl_match_ids = [26779, 26780]
+    # Get EPL matches for 2024 season, up to now...
+    epl_matches = get_understat_matches(league_id = "EPL", year = 2024)
 
-# Get data for each match
-matches_shot_data_list = get_understat_match_data_mutliple(epl_match_ids)
+    # Isolate match IDs
+    epl_match_ids = get_match_ids(epl_matches)
+    # epl_match_ids = [26779, 26780]
 
-# Create shots DataFrame
-shots_df = create_shots_df_multiple(matches_shot_data_list)
+    # Get data for each match
+    matches_shot_data_list = get_understat_match_data_mutliple(epl_match_ids)
 
-# Save dataframe
-shots_df.to_csv("EPL_2024_match_shots.csv", index = False)
+    # Create shots DataFrame
+    shots_df = create_shots_df_multiple(matches_shot_data_list)
+
+    # Save dataframe
+    shots_df.to_csv("EPL_2024_match_shots.csv", index = False)
